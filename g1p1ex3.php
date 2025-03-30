a0b053b44d484eacae028e58be28319375b692e6
Dana Corsani <164930463+DanaCorsani@users.noreply.github.com>
2025-03-30 18:15:38 -0300

    Add files via upload
    Algoritmos II Guia 1

---
+++ g1p1ex3.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 3</title>
+    <link rel="stylesheet" href="g1.css">
+</head>
+<body>
+
+    <h1> Algoritmos y Estructuras de Datos II </h1><br>
+    <h2>Paula Giaimo</h2><br>
+    <h3>GUIA de ejercicios en PHP</h3><br>
+    <h4>Primera Parte:</h4>
+    <p>3) Crear un programa que muestre los numeros del 150 al 0.</p><hr>
+    <h3>Numeros del 150 al 0: </h3>
+    <table>
+    
+    <?php
+        
+        for($i=150; $i>=0; $i--){
+            echo "<tr><td>$i</td></tr>";
+        }
+
+    ?>
+    
+    </table>
+
+</body>
+</html>
