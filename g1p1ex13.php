22e7d47efb88cbd07f8e66c5127356a10c17ecf0
DanaCorsani <danacorsani@gmail.com>
2025-08-13 07:13:16 -0300

    Add unfinished exercise 13

--- g1p1ex13.php
+++ g1p1ex13.php
@@ -12,7 +12,39 @@
     </header>
     <main>
         <form action="" method="POST">
+            <input type="hidden" name="fiveN">
+            <?php
+            #region first try
+            /*13) Ingresar 5 numeros en un formulario (utilizar un bucle para cargar los inputs). Pasar los valores del array a 5
+            variables individuales, $a, $b, $c, $d, $e (utillizando list). Luego mostrarlas
+            Ejemplo: Numeros ingresados 1,2,3,4,5
+            Debe mostrar:
+            La variable a vale= 1, la variable b vale 2, la variable c vale 3, la variable d vale 4 y la variable e vale 5*/
 
+            /*$ingresos=0;
+
+            while($ingresos<5){
+                ?>
+                <input type="number" name="">
+                <?php
+                $ingresos++;
+            }
+            if($ingresos=5){
+                ?>
+                <input type="submit" value="Cargar mis números">
+                <?php
+            }else{
+                echo"<br>Por favor ingrese 5 números";
+            }
+            */
+            #endregion
+            for($i=1, $i<=5, $i++){
+                echo "Numero $i: <input type='number' name='numeros[]' required"; #supuestamente con los corchetes despues del nombre, php lo recibe como array
+            }
+            ?>
+
+            <input type="submit" value="Procesar numeros">
+            
         </form>
     </main>
     <aside></aside>
@@ -20,12 +52,3 @@
 </body>
 </html>
 
-<?php
-
-/*13) Ingresar 5 numeros en un formulario (utilizar un bucle para cargar los inputs). Pasar los valores del array a 5
-variables individuales, $a, $b, $c, $d, $e (utillizando list). Luego mostrarlas
-Ejemplo: Numeros ingresados 1,2,3,4,5
-Debe mostrar:
-La variable a vale= 1, la variable b vale 2, la variable c vale 3, la variable d vale 4 y la variable e vale 5*/
-
-?>
