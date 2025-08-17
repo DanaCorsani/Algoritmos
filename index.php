240c4d32edbd0a95127c7345b8c89927647f62ee
DanaCorsani <danacorsani@gmail.com>
2025-08-17 00:16:07 -0300

    add index.php & changes to css

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index</title>
+    <link rel="stylesheet" href="./assets/css/g1.css">
+</head>
+<body>
+    <header>
+        <h1>Index Algoritmos y Estructuras de Datos II</h1>
+    </header>
+    <main>
+        <div class="guias">
+            <h2>Guía N°1 Parte 1</h2>
+            <button><a href="./g1p1ex1.php">Guía 1 Ejercicio 1</a></button>
+            <button><a href="./g1p1ex2.php">Guía 1 Ejercicio 2</a></button>
+            <button><a href="./g1p1ex3.php">Guía 1 Ejercicio 3</a></button>
+            <button><a href="./g1p1ex4.php">Guía 1 Ejercicio 4</a></button>
+            <button><a href="./g1p1ex5.php">Guía 1 Ejercicio 5</a></button>
+            <button><a href="./g1p1ex6.php">Guía 1 Ejercicio 6</a></button>
+            <button><a href="./g1p1ex7.php">Guía 1 Ejercicio 7</a></button>
+            <button><a href="./g1p1ex8.php">Guía 1 Ejercicio 8</a></button>
+            <button><a href="./g1p1ex9.php">Guía 1 Ejercicio 9</a></button>
+            <button><a href="./g1p1ex10.php">Guía 1 Ejercicio 10</a></button>
+            <button><a href="./g1p1ex11.php">Guía 1 Ejercicio 11</a></button>
+            <button><a href="./g1p1ex12.php">Guía 1 Ejercicio 12</a></button>
+            <button><a href="./g1p1ex13.php">Guía 1 Ejercicio 13</a></button>
+            <button><a href="./g1p1ex14.php">Guía 1 Ejercicio 14</a></button>
+            <button><a href="./g1p1ex15.php">Guía 1 Ejercicio 15</a></button>
+            <button><a href="./g1p1ex16.php">Guía 1 Ejercicio 16</a></button>
+            <button><a href="./g1p1ex17.php">Guía 1 Ejercicio 17</a></button>
+            <button><a href="./g1p1ex18.php">Guía 1 Ejercicio 18</a></button>
+            <button><a href="./g1p1ex19.php">Guía 1 Ejercicio 19</a></button>
+            <button><a href="./g1p1ex20.php">Guía 1 Ejercicio 20</a></button>
+        </div>
+    </main>
+</body>
+</html>
