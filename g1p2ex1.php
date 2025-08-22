c56810cf19e3bc7d48722d951c09e9e168819787
DanaCorsani <danacorsani@gmail.com>
2025-08-21 22:57:51 -0300

    add the first exercise from the guide 1 part 2, functions

--- g1p2ex1.php
+++ g1p2ex1.php
@@ -8,17 +8,25 @@
 </head>
 <body>
     <header>
-        <h1>Ejercicio</h1>
+        <h1>Parte 2 Ejercicio 1</h1>
     </header>
     <main>
         <form action="" method="POST">
             <input type="hidden" name="hidden">
+            Edad <input type="number" name="edad" required>
             <input type="submit" value="Enviar">
         </form>
     </main>
     <?php
-    
-    echo"";
+    /*1) EDAD: Crear una función calcularEdad que reciba una fecha de nacimiento y devuelva la edad actual de la
+persona.*/
+    if(isset($_POST['hidden'])){
+        $edad=$_POST['edad'];
+        echo"Usted ha ingresado la edad: ".$edad;
+        function($edad){
+            e
+        }
+    }
 
     ?>
 </body>
