89b226135e697e47617f7046da9fb4df224c93fa
DanaCorsani <danacorsani@gmail.com>
2025-08-25 22:01:15 -0300

    Add example

---
+++ g1ex1v2.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>g1ex1v2</title>
+</head>
+<body>
+    
+    <h1>g1ex1v2</h1>
+    <h2>que pide la profe?</h2>
+
+    <p>lista ordenada</p>
+    <ol>
+        <li>a</li>
+        <li>b</li>
+    </ol>
+
+    <p>lista desordenada</p>
+    <ul>
+        <li>a</li>
+        <li>b</li>
+    </ul>
+
+    <p>tablas</p>
+    <table>
+        <th><td>a</td><td>b</td></th>
+        <tr><td>1</td><td>2</td></tr>
+    </table>
+
+</body>
+</html>
